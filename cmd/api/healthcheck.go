ee583b0efc7019d3c2887c710acc650201b7edba
des-ant <57375593+des-ant@users.noreply.github.com>
2024-09-28 21:50:23 +1000

    Use json.Marshal to simplify encoding of JSON data

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // healthcheckHandler writes a plain-text response with information about
 // the application status, operating environment, and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This makes it easier to view in terminal.
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
