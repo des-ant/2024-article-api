75e759eecbcf0cd07c02635a9664262b5d06a8f6
des-ant <57375593+des-ant@users.noreply.github.com>
2024-09-28 15:26:33 +1000

    Create healcheck endpoint

--- README.md
+++ README.md
@@ -6,28 +6,48 @@
 Clone the project
 
 ```bash
-  git clone https://github.com/des-ant/2024-article-api.git
+git clone https://github.com/des-ant/2024-article-api.git
 ```
 
 Go to the project directory
 
 ```bash
-  cd 2024-article-api
+cd 2024-article-api
 ```
 
 Start the server
 
 ```bash
-  $ go run ./cmd/api
-  Hello world!
+$ go run ./cmd/api
+time=2024-09-28T15:13:02.746+10:00 level=INFO msg="starting server" addr=:4000 env=development
 ```
+
+
+<!-- Usage -->
+## :eyes: Usage
+
+While the server is running, visit [localhost:4000/v1/healthcheck](localhost:4000/v1/healthcheck) in your web browser.
+
+Alternatively, use `curl` to make the request from a terminal:
+```curl
+$ curl -i localhost:4000/v1/healthcheck
+HTTP/1.1 200 OK
+Date: Sat, 28 Sep 2024 05:25:21 GMT
+Content-Length: 58
+Content-Type: text/plain; charset=utf-8
+
+status: available
+environment: development
+version: 1.0.0
+```
+
 
 
 <!-- Roadmap -->
 ## :compass: Roadmap
 
 * [x] Set up project structure
-* [ ] Create basic HTTP Server
+* [x] Create basic HTTP Server
 * [ ] Add API endpoints
   * [ ] POST `/articles`
   * [ ] GET `/articles/{id}`
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Declare a string containing the application version number.
+// TODO: auto-generate at build time.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for our
+// application.
+// TODO: Add more settings as needed.
+// Currently includes:
+// - Network port for the server
+// - Operating environment (development, staging, production, etc.)
+// TODO: Read settings from command-line flags at startup.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for our HTTP handlers,
+// helpers, and middleware.
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+// parseFlags reads the value of the port and env command-line flags into the
+// config struct.
+func parseFlags(cfg *config) {
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	var cfg config
+
+	parseFlags(&cfg)
+
+	// Initialize a new structured logger which writes log entries to the standard
+	// out stream.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Declare an instance of the application struct, containing the config struct
+	// and the logger.
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux (router) and add a /v1/healthcheck route which
+	// dispatches requests to the healthcheckHandler method.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare a HTTP server which listens on the port provided in the config
+	// struct, uses the servemux we created above as the handler, has some
+	// sensible timeout settings and writes any log messages to the structured
+	// logger at Error level.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start the HTTP server.
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
