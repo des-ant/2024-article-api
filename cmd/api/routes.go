db5def1e71ca1506124fb3874ee672ea6978e6d1
des-ant <57375593+des-ant@users.noreply.github.com>
2024-09-29 11:50:15 +1000

    Use custom error handling in router to return JSON responses

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,6 +16,9 @@ const (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	app.addV1Routes(router)
 
 	return router
